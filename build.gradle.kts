52c753f7aff79c454544b0b2e181d989a64cace5
DanielTM999 <danielmelo101203@gmail.com>
2026-09-18 20:08:11 -0300

    ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 val upstreamVersion = rootProject.projectDir.resolve("VERSION").readText().trim()
 val orionRevision = providers.gradleProperty("orionRevision").getOrElse("1")
+val publishGroup = providers.gradleProperty("group").getOrElse("dtm.ide")
+val publishVersion = providers.gradleProperty("version").getOrElse("$upstreamVersion-orion.$orionRevision")
 
 allprojects {
-  version = "$upstreamVersion-orion.$orionRevision"
-  group = "dtm.ide"
+  version = publishVersion
+  group = publishGroup
   layout.buildDirectory = rootProject.projectDir.resolve(".gradleBuild/" + project.name)
 }
 
